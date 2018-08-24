4e8d566e7fdbfbaf32c898fbc4a6001caa7e52d1
Swen Thümmler <st@jpc.de>
2018-08-24 15:40:46 +0200

    be transaction friendly   use jdbcTemplate (which internally uses DataSourceUtils.getConnection())   use @Transactional with Propagation.REQUIRES_NEW

--- providers/jdbc/shedlock-provider-jdbc-template/src/main/java/net/javacrumbs/shedlock/provider/jdbctemplate/JdbcTemplateStorageAccessor.java
+++ providers/jdbc/shedlock-provider-jdbc-template/src/main/java/net/javacrumbs/shedlock/provider/jdbctemplate/JdbcTemplateStorageAccessor.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2009-2017 the original author or authors.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,47 +15,74 @@
  */
 package net.javacrumbs.shedlock.provider.jdbctemplate;
 
-import net.javacrumbs.shedlock.provider.jdbc.internal.AbstractJdbcStorageAccessor;
-import org.springframework.dao.DataAccessException;
+import net.javacrumbs.shedlock.core.LockConfiguration;
+import net.javacrumbs.shedlock.support.AbstractStorageAccessor;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.jdbc.support.SQLErrorCodeSQLExceptionTranslator;
-import org.springframework.jdbc.support.SQLExceptionTranslator;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.sql.DataSource;
-import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.Instant;
+
+import static java.util.Objects.requireNonNull;
 
 /**
- * Spring JdbcTemplate based implementation which at the end does not use much from JdbcTemplate except
- * exception translator. The reason is that we rely on atomic JDBC operations and thus do not want to
- * participate in transactions. It's not easy to not use transactions with JdbcTemplate and since we were using
- * only exception translation, it makes sense to skip JdbcTemplate completely.
+ * Spring JdbcTemplate based implementation usable in JTA environment
  */
-class JdbcTemplateStorageAccessor extends AbstractJdbcStorageAccessor {
-    private final SQLExceptionTranslator exceptionTranslator;
+class JdbcTemplateStorageAccessor extends AbstractStorageAccessor {
+	private final String tableName;
+	private final JdbcTemplate jdbcTemplate;
 
-    JdbcTemplateStorageAccessor(DataSource dataSource, String tableName) {
-        super(dataSource, tableName);
-        this.exceptionTranslator = new SQLErrorCodeSQLExceptionTranslator(dataSource);
-    }
+	JdbcTemplateStorageAccessor(JdbcTemplate jdbcTemplate, String tableName) {
+		this.jdbcTemplate = requireNonNull(jdbcTemplate, "jdbcTemplate can not be null");
+		this.tableName = requireNonNull(tableName, "tableName can not be null");
+	}
 
-    @Override
-    protected void handleInsertionException(String sql, SQLException e) {
-        DataAccessException translatedException = exceptionTranslator.translate("InsertLock", sql, e);
-        if (translatedException instanceof DataIntegrityViolationException) {
-            // lock record already exists
-            // DuplicateKeyException is not enough for Vertica
-        } else {
-            throw translatedException;
-        }
-    }
+	@Override
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
+	public boolean insertRecord(LockConfiguration lockConfiguration) {
+		String sql = "INSERT INTO " + tableName + "(name, lock_until, locked_at, locked_by) VALUES(?, ?, ?, ?)";
+		try
+		{
+			int insertedRows = jdbcTemplate.update(sql, preparedStatement -> {
+				preparedStatement.setString(1, lockConfiguration.getName());
+				preparedStatement.setTimestamp(2, Timestamp.from(lockConfiguration.getLockAtMostUntil()));
+				preparedStatement.setTimestamp(3, Timestamp.from(Instant.now()));
+				preparedStatement.setString(4, getHostname());
+			});
+			return insertedRows > 0;
+		}
+		catch (DataIntegrityViolationException e)
+		{
+			return false;
+		}
+	}
 
-    @Override
-    protected void handleUpdateException(String sql, SQLException e) {
-        throw exceptionTranslator.translate("UpdateLock", sql, e);
-    }
+	@Override
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
+	public boolean updateRecord(LockConfiguration lockConfiguration) {
+		String sql = "UPDATE " + tableName
+		             + " SET lock_until = ?, locked_at = ?, locked_by = ? WHERE name = ? AND lock_until <= ?";
+		int updatedRows = jdbcTemplate.update(sql, statement -> {
+			Timestamp now = Timestamp.from(Instant.now());
+			statement.setTimestamp(1, Timestamp.from(lockConfiguration.getLockAtMostUntil()));
+			statement.setTimestamp(2, now);
+			statement.setString(3, getHostname());
+			statement.setString(4, lockConfiguration.getName());
+			statement.setTimestamp(5, now);
+		});
+		return updatedRows > 0;
+	}
 
-    @Override
-    protected void handleUnlockException(String sql, SQLException e) {
-        throw exceptionTranslator.translate("Unlock", sql, e);
-    }
+	@Override
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
+	public void unlock(LockConfiguration lockConfiguration) {
+		String sql = "UPDATE " + tableName + " SET lock_until = ? WHERE name = ?";
+		jdbcTemplate.update(sql, statement -> {
+			statement.setTimestamp(1, Timestamp.from(lockConfiguration.getUnlockTime()));
+			statement.setString(2, lockConfiguration.getName());
+		});
+	}
+
 }
