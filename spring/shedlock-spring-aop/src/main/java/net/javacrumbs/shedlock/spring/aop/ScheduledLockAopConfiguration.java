296e6fa311babf4fe5b69ee9a3656d0f4e4f3305
Lukas Krecan <lukas@krecan.net>
2018-10-08 21:36:32 +0200

    Refactored

--- spring/shedlock-spring-aop/src/main/java/net/javacrumbs/shedlock/spring/aop/ScheduledLockAopConfiguration.java
+++ spring/shedlock-spring-aop/src/main/java/net/javacrumbs/shedlock/spring/aop/ScheduledLockAopConfiguration.java
@@ -19,17 +19,24 @@ import net.javacrumbs.shedlock.core.LockConfiguration;
 import net.javacrumbs.shedlock.core.LockingTaskExecutor;
 import net.javacrumbs.shedlock.core.SchedulerLock;
 import net.javacrumbs.shedlock.spring.internal.SpringLockConfigurationExtractor;
+import org.aopalliance.aop.Advice;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
-import org.springframework.beans.factory.FactoryBean;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.framework.autoproxy.AbstractBeanFactoryAwareAdvisingPostProcessor;
+import org.springframework.aop.support.AbstractPointcutAdvisor;
+import org.springframework.aop.support.annotation.AnnotationMatchingPointcut;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.EmbeddedValueResolverAware;
 import org.springframework.util.StringValueResolver;
 
 import java.time.temporal.TemporalAmount;
 
-class ScheduledLockAopConfiguration implements ScheduledLockConfiguration, EmbeddedValueResolverAware, FactoryBean<ScheduledLockAopConfiguration.ScheduledLockAspect> {
+class ScheduledLockAopConfiguration extends AbstractBeanFactoryAwareAdvisingPostProcessor implements ScheduledLockConfiguration, EmbeddedValueResolverAware {
     private final LockingTaskExecutor lockingTaskExecutor;
     private final TemporalAmount defaultLockAtMostFor;
     private final TemporalAmount defaultLockAtLeastFor;
@@ -51,40 +58,55 @@ class ScheduledLockAopConfiguration implements ScheduledLockConfiguration, Embed
         this.resolver = resolver;
     }
 
-    @Override
-    public ScheduledLockAspect getObject() {
-        return new ScheduledLockAspect(new SpringLockConfigurationExtractor(defaultLockAtMostFor, defaultLockAtLeastFor, resolver), lockingTaskExecutor);
-    }
 
     @Override
-    public Class<?> getObjectType() {
-        return ScheduledLockAspect.class;
+    public void setBeanFactory(BeanFactory beanFactory) {
+        super.setBeanFactory(beanFactory);
+        this.advisor = new ScheduledLockAdvisor(new SpringLockConfigurationExtractor(defaultLockAtMostFor, defaultLockAtLeastFor, resolver), lockingTaskExecutor);
     }
 
-    @Aspect
-    static class ScheduledLockAspect {
-        private final SpringLockConfigurationExtractor lockConfigurationExtractor;
-        private final LockingTaskExecutor lockingTaskExecutor;
+    static class ScheduledLockAdvisor extends AbstractPointcutAdvisor {
+        private final AnnotationMatchingPointcut pointcut = AnnotationMatchingPointcut.forMethodAnnotation(SchedulerLock.class);
+        private final Advice advice;
 
-        private ScheduledLockAspect(SpringLockConfigurationExtractor lockConfigurationExtractor, LockingTaskExecutor lockingTaskExecutor) {
-            this.lockConfigurationExtractor = lockConfigurationExtractor;
-            this.lockingTaskExecutor = lockingTaskExecutor;
+        private ScheduledLockAdvisor(SpringLockConfigurationExtractor lockConfigurationExtractor, LockingTaskExecutor lockingTaskExecutor) {
+            this.advice = new LockingInterceptor(lockConfigurationExtractor, lockingTaskExecutor);
         }
 
         /**
-          * Supports only void method since we do not know what to return if the task is locked.
-          */
-         @Around("@annotation(schedulerLockAnnotation)")
-         void lockIt(ProceedingJoinPoint pjp, SchedulerLock schedulerLockAnnotation) throws Throwable {
-             if (pjp.getSignature() instanceof MethodSignature) {
-                 Class<?> returnType = ((MethodSignature) pjp.getSignature()).getMethod().getReturnType();
-                 if (!void.class.equals(returnType) && !Void.class.equals(returnType)) {
-                     throw new LockingNotSupportedException();
-                 }
-             }
+         * Get the Pointcut that drives this advisor.
+         */
+        @Override
+        public Pointcut getPointcut() {
+            return pointcut;
+        }
 
-             LockConfiguration lockConfiguration = lockConfigurationExtractor.getLockConfiguration(schedulerLockAnnotation);
-             lockingTaskExecutor.executeWithLock((LockingTaskExecutor.Task) pjp::proceed, lockConfiguration);
-         }
+        @Override
+        public Advice getAdvice() {
+            return advice;
+        }
+
+        private static class LockingInterceptor implements MethodInterceptor {
+            private final SpringLockConfigurationExtractor lockConfigurationExtractor;
+            private final LockingTaskExecutor lockingTaskExecutor;
+
+            LockingInterceptor(SpringLockConfigurationExtractor lockConfigurationExtractor, LockingTaskExecutor lockingTaskExecutor) {
+                this.lockConfigurationExtractor = lockConfigurationExtractor;
+                this.lockingTaskExecutor = lockingTaskExecutor;
+            }
+
+            @Override
+            public Object invoke(MethodInvocation invocation) throws Throwable {
+
+                Class<?> returnType = invocation.getMethod().getReturnType();
+                if (!void.class.equals(returnType) && !Void.class.equals(returnType)) {
+                    throw new LockingNotSupportedException();
+                }
+
+                LockConfiguration lockConfiguration = lockConfigurationExtractor.getLockConfiguration(invocation.getThis(), invocation.getMethod()).get();
+                lockingTaskExecutor.executeWithLock((LockingTaskExecutor.Task) invocation.proceed(), lockConfiguration);
+                return null;
+            }
+        }
     }
 }
