26e5cd89720021e5cd7370257425e770128ad53e
Lukas Krecan <lukas@krecan.net>
2019-01-20 19:40:17 +0100

    TestNG test

---
+++ spring/shedlock-testng-test/src/test/java/net/javacrumbs/shedlock/test/boot/IntegrationTest.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright 2009-2018 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.javacrumbs.shedlock.test.boot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import javax.sql.DataSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class IntegrationTest extends AbstractTestNGSpringContextTests {
+    @Autowired
+    private DataSource dataSource;
+
+    @BeforeClass(alwaysRun = true)
+    public void initDb() {
+        assertThat(dataSource).isNotNull();
+    }
+
+    @Test
+    public void testScheduler() {
+        assertThat(dataSource).isNotNull();
+    }
+}
