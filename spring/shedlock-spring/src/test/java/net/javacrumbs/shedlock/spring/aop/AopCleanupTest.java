2e85e782eb513bb6ef68b773364e5a5a007ac82d
Lukas Krecan <lukas@krecan.net>
2019-09-07 10:09:03 +0200

    Cleanup test

---
+++ spring/shedlock-spring/src/test/java/net/javacrumbs/shedlock/spring/aop/AopCleanupTest.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright 2009-2018 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.javacrumbs.shedlock.spring.aop;
+
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.core.SchedulerLock;
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
+import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+public class AopCleanupTest {
+    @Test
+    public void shouldCloseTaskExecutor() {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+        context.start();
+        context.close();
+        assertThat(context.isActive()).isFalse();
+    }
+
+
+    @Configuration
+    @EnableScheduling
+    @EnableSchedulerLock(defaultLockAtMostFor = "PT30S")
+    static class Config {
+        private static LockProvider lockProvider = mock(LockProvider.class);
+
+        @Bean
+        public LockProvider lockProvider() {
+            return lockProvider;
+        }
+
+        @Scheduled(fixedRate = 10_000)
+        @SchedulerLock(name = "task")
+        public void task() {
+
+        }
+    }
+}
