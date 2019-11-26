57eeddb2a69b6acfd02eeb0b8ec8df1aa7bea759
Lukas Krecan <lukas@krecan.net>
2019-11-26 20:24:20 +0100

    Micronaut test added

--- micronaut/micronaut-test/src/main/java/micronaut/test/Application.java
+++ micronaut/micronaut-test/src/main/java/micronaut/test/Application.java
@@ -1,18 +1,10 @@
 package micronaut.test;
 
 import io.micronaut.runtime.Micronaut;
-import io.micronaut.scheduling.annotation.Scheduled;
-import net.javacrumbs.shedlock.micronaut.SchedulerLock;
 
 public class Application {
 
     public static void main(String[] args) {
         Micronaut.run(Application.class);
-    }
-
-    @Scheduled(fixedRate = "1s")
-    @SchedulerLock
-    public void task() {
-        System.out.println("Task");
     }
 }
