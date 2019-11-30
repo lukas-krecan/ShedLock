2d668ea7f2b7822cb5c48f77293a898e9e7dd2fb
Lukas Krecan <lukas@krecan.net>
2019-11-30 10:09:45 +0100

    Do not allow empty name in Micronaut annotation

--- micronaut/shedlock-micronaut/src/main/java/net/javacrumbs/shedlock/micronaut/SchedulerLock.java
+++ micronaut/shedlock-micronaut/src/main/java/net/javacrumbs/shedlock/micronaut/SchedulerLock.java
@@ -38,21 +38,21 @@ public @interface SchedulerLock {
     /**
      * Lock name.
      */
-    String name() default "";
+    String name();
 
     /**
-     * How long (in ms) the lock should be kept in case the machine which obtained the lock died before releasing it.
-     * This is just a fallback, under normal circumstances the lock is released as soon the tasks finishes. Can be a format
+     * How long the lock should be kept in case the machine which obtained the lock died before releasing it.
+     * This is just a fallback, under normal circumstances the lock is released as soon the tasks finishes. Can be any format
      * supported by <a href="https://docs.micronaut.io/latest/guide/config.html#_duration_conversion">Duration Conversion</a>
      * <p>
      */
     String lockAtMostFor() default "";
 
     /**
-     * The lock will be held at least for X millis. Can be used if you really need to execute the task
+     * The lock will be held at least for this period of time. Can be used if you really need to execute the task
      * at most once in given period of time. If the duration of the task is shorter than clock difference between nodes, the task can
      * be theoretically executed more than once (one node after another). By setting this parameter, you can make sure that the
-     * lock will be kept at least for given period of time. Can be a format
+     * lock will be kept at least for given period of time. Can be any format
      * supported by <a href="https://docs.micronaut.io/latest/guide/config.html#_duration_conversion">Duration Conversion</a>
      */
     String lockAtLeastFor() default "";
