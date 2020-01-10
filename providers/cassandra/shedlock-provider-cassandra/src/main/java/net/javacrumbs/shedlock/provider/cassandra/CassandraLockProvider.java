b37dddea5caf43da7e1e66850a6feeacb79d8968
Lukas Krecan <lukas@krecan.net>
2020-01-10 19:50:40 +0100

    Simplify CassandraStorageAccessor

--- providers/cassandra/shedlock-provider-cassandra/src/main/java/net/javacrumbs/shedlock/provider/cassandra/CassandraLockProvider.java
+++ providers/cassandra/shedlock-provider-cassandra/src/main/java/net/javacrumbs/shedlock/provider/cassandra/CassandraLockProvider.java
@@ -14,29 +14,8 @@ import org.jetbrains.annotations.NotNull;
  * </pre>
  */
 public class CassandraLockProvider extends StorageBasedLockProvider {
-
-    static final String DEFAULT_CONCACT_POINT = "localhost";
-    static final int DEFAULT_PORT = 9042;
-    static final String DEFAULT_DATACENTER = "datacenter1";
-    static final String DEFAULT_KEYSPACE = "shedlock";
     static final String DEFAULT_TABLE = "lock";
 
-    /**
-     * Default configuration contact point is localhost at port 9042 at datacenter1 using keyspace shedlock anb table lock
-     */
-    public CassandraLockProvider() {
-        super(new CassandraStorageAccessor(DEFAULT_CONCACT_POINT, DEFAULT_PORT, DEFAULT_DATACENTER, DEFAULT_KEYSPACE, DEFAULT_TABLE));
-    }
-
-    public CassandraLockProvider(@NotNull String contactPoint, @NotNull int port, @NotNull String datacenter, @NotNull String keyspace, @NotNull String table) {
-        super(new CassandraStorageAccessor(contactPoint, port, datacenter, keyspace, table));
-    }
-
-    /**
-     * Using default table lock
-     *
-     * @param cqlSession
-     */
     public CassandraLockProvider(@NotNull CqlSession cqlSession) {
         super(new CassandraStorageAccessor(cqlSession, DEFAULT_TABLE));
     }
