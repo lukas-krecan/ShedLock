4a1013d375e790e29ead4f2e678cb91ef5555a17
Lukas Krecan <lukas@krecan.net>
2020-01-10 19:53:52 +0100

    Simplify Cassandra Lock

--- providers/cassandra/shedlock-provider-cassandra/src/main/java/net/javacrumbs/shedlock/provider/cassandra/Lock.java
+++ providers/cassandra/shedlock-provider-cassandra/src/main/java/net/javacrumbs/shedlock/provider/cassandra/Lock.java
@@ -2,49 +2,26 @@ package net.javacrumbs.shedlock.provider.cassandra;
 
 import java.time.Instant;
 
-public class Lock {
+class Lock {
+    private final Instant lockUntil;
+    private final Instant lockedAt;
+    private final String lockedBy;
 
-    private String name;
-    private Instant lockUntil;
-    private Instant lockedAt;
-    private String lockedBy;
-
-    public Lock(String name, Instant lockUntil, Instant lockedAt, String lockedBy) {
-        this.name = name;
+    Lock(Instant lockUntil, Instant lockedAt, String lockedBy) {
         this.lockUntil = lockUntil;
         this.lockedAt = lockedAt;
         this.lockedBy = lockedBy;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Instant getLockUntil() {
+    Instant getLockUntil() {
         return lockUntil;
     }
 
-    public void setLockUntil(Instant lockUntil) {
-        this.lockUntil = lockUntil;
-    }
-
-    public Instant getLockedAt() {
+    Instant getLockedAt() {
         return lockedAt;
     }
 
-    public void setLockedAt(Instant lockedAt) {
-        this.lockedAt = lockedAt;
-    }
-
-    public String getLockedBy() {
+    String getLockedBy() {
         return lockedBy;
-    }
-
-    public void setLockedBy(String lockedBy) {
-        this.lockedBy = lockedBy;
     }
 }
