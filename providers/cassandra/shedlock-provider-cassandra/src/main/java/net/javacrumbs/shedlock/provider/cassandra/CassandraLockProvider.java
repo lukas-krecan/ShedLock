e43fc3bce81dca14bd79761e9d9e9336ff4a2d29
Lukas Krecan <lukas@krecan.net>
2020-01-10 20:07:39 +0100

    Allow to set consistency level

--- providers/cassandra/shedlock-provider-cassandra/src/main/java/net/javacrumbs/shedlock/provider/cassandra/CassandraLockProvider.java
+++ providers/cassandra/shedlock-provider-cassandra/src/main/java/net/javacrumbs/shedlock/provider/cassandra/CassandraLockProvider.java
@@ -1,5 +1,6 @@
 package net.javacrumbs.shedlock.provider.cassandra;
 
+import com.datastax.oss.driver.api.core.ConsistencyLevel;
 import com.datastax.oss.driver.api.core.CqlSession;
 import net.javacrumbs.shedlock.support.StorageBasedLockProvider;
 import org.jetbrains.annotations.NotNull;
@@ -17,10 +18,10 @@ public class CassandraLockProvider extends StorageBasedLockProvider {
     static final String DEFAULT_TABLE = "lock";
 
     public CassandraLockProvider(@NotNull CqlSession cqlSession) {
-        super(new CassandraStorageAccessor(cqlSession, DEFAULT_TABLE));
+        super(new CassandraStorageAccessor(cqlSession, DEFAULT_TABLE, ConsistencyLevel.QUORUM));
     }
 
-    public CassandraLockProvider(@NotNull CqlSession cqlSession, @NotNull String table) {
-        super(new CassandraStorageAccessor(cqlSession, table));
+    public CassandraLockProvider(@NotNull CqlSession cqlSession, @NotNull String table, @NotNull ConsistencyLevel consistencyLevel) {
+        super(new CassandraStorageAccessor(cqlSession, table, consistencyLevel));
     }
 }
