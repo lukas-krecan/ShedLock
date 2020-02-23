608543b67a9b33560a5b89759e2c72e08c95e669
Lukas Krecan <lukas.krecan@adleritech.net>
2020-02-23 18:15:24 +0100

    Make JDBC test more stable

--- providers/jdbc/shedlock-test-support-jdbc/src/main/java/net/javacrumbs/shedlock/test/support/jdbc/AbstractJdbcLockProviderIntegrationTest.java
+++ providers/jdbc/shedlock-test-support-jdbc/src/main/java/net/javacrumbs/shedlock/test/support/jdbc/AbstractJdbcLockProviderIntegrationTest.java
@@ -50,7 +50,7 @@ public abstract class AbstractJdbcLockProviderIntegrationTest extends AbstractSt
     @Override
     protected void assertUnlocked(String lockName) {
         Instant lockedUntil = getLockedUntil(lockName);
-        assertThat(lockedUntil).isBeforeOrEqualTo(Instant.now().truncatedTo(ChronoUnit.MILLIS));
+        assertThat(lockedUntil).isBeforeOrEqualTo(Instant.now().truncatedTo(ChronoUnit.MILLIS).plusMillis(1));
     }
 
     private Instant getLockedUntil(String lockName) {
