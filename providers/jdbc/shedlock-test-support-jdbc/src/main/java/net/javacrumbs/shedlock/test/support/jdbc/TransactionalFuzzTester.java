d1cdab803ae25b4e30102b166342109427f262b3
Lukáš Křečan <lukas.krecan@productboard.com>
2020-05-10 18:05:52 +0200

    Fuzz test with two locks

--- providers/jdbc/shedlock-test-support-jdbc/src/main/java/net/javacrumbs/shedlock/test/support/jdbc/TransactionalFuzzTester.java
+++ providers/jdbc/shedlock-test-support-jdbc/src/main/java/net/javacrumbs/shedlock/test/support/jdbc/TransactionalFuzzTester.java
@@ -15,6 +15,7 @@
  */
 package net.javacrumbs.shedlock.test.support.jdbc;
 
+import net.javacrumbs.shedlock.core.LockConfiguration;
 import net.javacrumbs.shedlock.core.LockProvider;
 import net.javacrumbs.shedlock.test.support.FuzzTester;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
@@ -28,9 +29,9 @@ public class TransactionalFuzzTester {
     public static void fuzzTestShouldWorkWithTransaction(LockProvider lockProvider, DataSource dataSource) throws ExecutionException, InterruptedException {
         new FuzzTester(lockProvider) {
             @Override
-            protected Void task(int iterations) {
+            protected Void task(int iterations, Job job) {
                 TransactionTemplate transactionTemplate = new TransactionTemplate(new DataSourceTransactionManager(dataSource));
-                return transactionTemplate.execute(status -> super.task(iterations));
+                return transactionTemplate.execute(status -> super.task(iterations, job));
             }
 
             @Override
--- shedlock-test-support/src/main/java/net/javacrumbs/shedlock/test/support/FuzzTester.java
+++ shedlock-test-support/src/main/java/net/javacrumbs/shedlock/test/support/FuzzTester.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2009-2020 the original author or authors.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -46,9 +46,6 @@ public class FuzzTester {
 
     private final LockProvider lockProvider;
 
-    private int counter;
-    private final LockConfiguration config = new LockConfiguration("lock", Duration.of(5, ChronoUnit.MINUTES), Duration.of(5, ChronoUnit.MILLIS));
-
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
     public FuzzTester(LockProvider lockProvider) {
@@ -59,11 +56,16 @@ public class FuzzTester {
         ExecutorService executor = Executors.newFixedThreadPool(THREADS);
         int[] iterations = range(0, THREADS).map(i -> ITERATIONS).toArray();
         iterations[0] = SHORT_ITERATION; // short task to simulate MySql issues
+        Job job1 = new Job("lock1");
+        Job job2 = new Job("lock2");
 
-        List<Callable<Void>> tasks = range(0, THREADS).mapToObj(i -> (Callable<Void>) () -> this.task(iterations[i])).collect(toList());
+        List<Callable<Void>> tasks = range(0, THREADS).mapToObj(i -> (Callable<Void>) () ->
+            this.task(iterations[i], i % 2 == 0 ? job1 : job2)).collect(toList()
+        );
         waitForIt(executor.invokeAll(tasks));
-
-        assertThat(counter).isEqualTo((THREADS - 1) * ITERATIONS + SHORT_ITERATION);
+        
+        assertThat(job2.getCounter()).isEqualTo(THREADS / 2 * ITERATIONS);
+        assertThat(job1.getCounter()).isEqualTo((THREADS / 2 - 1) * ITERATIONS + SHORT_ITERATION);
     }
 
     private void waitForIt(List<Future<Void>> futures) throws InterruptedException, ExecutionException {
@@ -72,18 +74,18 @@ public class FuzzTester {
         }
     }
 
-    protected Void task(int iterations) {
+    protected Void task(int iterations, Job job) {
         try {
-            for (int i = 0; i < iterations; ) {
-                Optional<SimpleLock> lock = lockProvider.lock(config);
+            for (int i = 0; i < iterations;) {
+                Optional<SimpleLock> lock = lockProvider.lock(job.getLockConfiguration());
                 if (lock.isPresent()) {
-                    int n = counter;
+                    int n = job.getCounter();
                     if (shouldLog()) logger.debug("action=getLock value={} i={}", n, i);
                     sleep();
                     if (shouldLog()) logger.debug("action=setCounter value={} i={}", n + 1, i);
-                    counter = n + 1;
-                    i++;
+                    job.setCounter(n + 1);
                     lock.get().unlock();
+                    i++;
                 }
             }
             logger.debug("action=finished");
@@ -106,4 +108,30 @@ public class FuzzTester {
         }
     }
 
+    protected static class Job {
+        private final String lockName;
+        private int counter = 0;
+
+        Job(String lockName) {
+            this.lockName = lockName;
+        }
+
+        public LockConfiguration getLockConfiguration() {
+            return new LockConfiguration(
+                lockName,
+                Duration.of(5, ChronoUnit.MINUTES),
+                Duration.of(5, ChronoUnit.MILLIS)
+            );
+        }
+
+        public int getCounter() {
+            return counter;
+        }
+
+        public void setCounter(int counter) {
+            this.counter = counter;
+        }
+    }
 }
+
+
