95f06c52b7152dbad11c69b98716efeefca09405
Lukas Krecan <lukas@krecan.net>
2020-05-20 18:52:23 +0200

    Test postgres 9

---
+++ providers/jdbc/shedlock-provider-jdbc-template/src/test/java/net/javacrumbs/shedlock/provider/jdbctemplate/Postgres9JdbcTemplateLockProviderIntegrationTest.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright 2009-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.javacrumbs.shedlock.provider.jdbctemplate;
+
+import net.javacrumbs.shedlock.core.ClockProvider;
+import net.javacrumbs.shedlock.core.LockConfiguration;
+import net.javacrumbs.shedlock.support.StorageBasedLockProvider;
+import net.javacrumbs.shedlock.test.support.jdbc.AbstractPostgres9JdbcLockProviderIntegrationTest;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.sql.Timestamp;
+import java.time.Clock;
+import java.time.Instant;
+import java.util.TimeZone;
+
+import static net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider.Configuration.builder;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Postgres9JdbcTemplateLockProviderIntegrationTest extends AbstractPostgres9JdbcLockProviderIntegrationTest {
+    @Override
+    protected StorageBasedLockProvider getLockProvider() {
+        return new JdbcTemplateLockProvider(getDatasource());
+    }
+    @AfterEach
+    void resetClock() {
+        ClockProvider.setClock(Clock.systemDefaultZone());
+    }
+
+    @Test
+    void shouldHonorTimezone() {
+        TimeZone timezone = TimeZone.getTimeZone("America/Los_Angeles");
+
+        Instant lockUntil = Instant.parse("2020-04-10T17:30:00Z");
+        ClockProvider.setClock(Clock.fixed(lockUntil.minusSeconds(10), timezone.toZoneId()));
+
+        TimeZone originalTimezone = TimeZone.getDefault();
+
+
+        DataSource datasource = getDatasource();
+
+        TimeZone.setDefault(timezone);
+
+        try {
+            JdbcTemplate jdbcTemplate = new JdbcTemplate(datasource);
+            jdbcTemplate.execute("CREATE TABLE shedlock_tz(name VARCHAR(64), lock_until TIMESTAMP WITH TIME ZONE, locked_at TIMESTAMP WITH TIME ZONE, locked_by  VARCHAR(255), PRIMARY KEY (name))");
+
+            JdbcTemplateLockProvider provider = new JdbcTemplateLockProvider(builder()
+                .withJdbcTemplate(new JdbcTemplate(datasource))
+                .withTableName("shedlock_tz")
+                .withTimeZone(timezone)
+                .build());
+
+
+            provider.lock(new LockConfiguration("timezone_test", lockUntil));
+            new JdbcTemplate(datasource).query("SELECT * FROM shedlock_tz where name='timezone_test'", rs -> {
+                Timestamp timestamp = rs.getTimestamp("lock_until");
+                assertThat(timestamp.getTimezoneOffset()).isEqualTo(7 * 60);
+                assertThat(timestamp.toInstant()).isEqualTo(lockUntil);
+            });
+        } finally {
+            TimeZone.setDefault(originalTimezone);
+        }
+    }
+}
---
+++ providers/jdbc/shedlock-test-support-jdbc/src/main/java/net/javacrumbs/shedlock/test/support/jdbc/Postgres9Config.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright 2009-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.javacrumbs.shedlock.test.support.jdbc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+public final class Postgres9Config implements DbConfig {
+
+    private static final String TEST_SCHEMA_NAME = "shedlock_test";
+    private MyPostgreSQLContainer postgres;
+    private static final Logger logger = LoggerFactory.getLogger(Postgres9Config.class);
+
+    public void startDb() {
+        postgres = new MyPostgreSQLContainer()
+            .withDatabaseName(TEST_SCHEMA_NAME)
+            .withUsername("SA")
+            .withPassword("pass")
+            .withLogConsumer(outputFrame -> logger.debug(outputFrame.getUtf8String()));
+        postgres.start();
+    }
+
+    public void shutdownDb() {
+        postgres.stop();
+    }
+
+    public String getJdbcUrl() {
+        return postgres.getJdbcUrl();
+
+    }
+
+    @Override
+    public String getUsername() {
+        return postgres.getUsername();
+    }
+
+    @Override
+    public String getPassword() {
+        return postgres.getPassword();
+    }
+
+    private static class MyPostgreSQLContainer extends PostgreSQLContainer<MyPostgreSQLContainer> {
+        MyPostgreSQLContainer() {
+            super("postgres:9.6-alpine");
+        }
+    }
+}
