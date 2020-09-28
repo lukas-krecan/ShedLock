714c794bde2d61c5688eec395d6f610cd44f8fce
Lukas Krecan <lukas@krecan.net>
2020-09-28 12:07:46 +0200

    Spring Kotlin test using Boot 2.3

--- spring/test/shedlock-springboot-kotlin-test/src/test/kotlin/net/javacrumbs/shedlock/test/boot/IntegrationTest.kt
+++ spring/test/shedlock-springboot-kotlin-test/src/test/kotlin/net/javacrumbs/shedlock/test/boot/IntegrationTest.kt
@@ -21,15 +21,15 @@ import net.javacrumbs.shedlock.core.LockConfiguration
 import net.javacrumbs.shedlock.core.LockProvider
 import org.assertj.core.api.Assertions.assertThat
 import org.awaitility.Awaitility.await
-import org.junit.Test
-import org.junit.runner.RunWith
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.ArgumentCaptor
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
-import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @SpringBootTest
-@RunWith(SpringRunner::class)
+@ExtendWith(SpringExtension::class)
 class IntegrationTest {
     @MockBean
     private lateinit var lockProvider: LockProvider
