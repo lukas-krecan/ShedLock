f2a49e8ddaed89c456a2f93bca6a5b8ee473d675
Lukas Krecan <lukas@krecan.net>
2021-03-20 10:24:47 +0100

    Added spring-boot-future-test

---
+++ spring/test/shedlock-springboot-future-test/src/test/java/net/javacrumbs/shedlock/test/boot/IntegrationTest.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright 2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.javacrumbs.shedlock.test.boot;
+
+import net.javacrumbs.shedlock.core.LockConfiguration;
+import net.javacrumbs.shedlock.core.LockProvider;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest
+public class IntegrationTest {
+    @MockBean
+    private LockProvider lockProvider;
+
+    @Test
+    public void testScheduler() {
+        await().untilAsserted(() -> {
+            ArgumentCaptor<LockConfiguration> configCaptor = ArgumentCaptor.forClass(LockConfiguration.class);
+            verify(lockProvider, atLeastOnce()).lock(configCaptor.capture());
+            assertThat(configCaptor.getValue().getName()).isEqualTo("reportCurrentTime");
+        });
+    }
+}
