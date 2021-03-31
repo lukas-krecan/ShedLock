9a8dad94b0ec18a44cf33bce00468e46362b99ff
Lukas Krecan <lukas@krecan.net>
2021-03-31 20:33:37 +0200

    Make Hazelcast test failure more infomative

--- providers/hazelcast/shedlock-provider-hazelcast4/src/test/java/net/javacrumbs/shedlock/provider/hazelcast4/HazelcastLockProviderClusterTest.java
+++ providers/hazelcast/shedlock-provider-hazelcast4/src/test/java/net/javacrumbs/shedlock/provider/hazelcast4/HazelcastLockProviderClusterTest.java
@@ -25,12 +25,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
+import java.time.Instant;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
 import static java.time.temporal.ChronoUnit.SECONDS;
 import static net.javacrumbs.shedlock.core.ClockProvider.now;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 
 public class HazelcastLockProviderClusterTest {
 
@@ -76,10 +78,18 @@ public class HazelcastLockProviderClusterTest {
         assertThat(lock21).isEmpty();
         lock11.get().unlock();
         lock22.get().unlock();
-        Optional<SimpleLock> lock12Bis = lockProvider2.lock(simpleLockConfig(LOCK_NAME_1));
-        assertThat(lock12Bis).isNotEmpty();
-        Optional<SimpleLock> lock21Bis = lockProvider1.lock(simpleLockConfig(LOCK_NAME_2));
-        assertThat(lock21Bis).isNotEmpty();
+        assertUnlocked(lockProvider2, LOCK_NAME_1);
+        assertUnlocked(lockProvider1, LOCK_NAME_2);
+    }
+
+    private void assertUnlocked(HazelcastLockProvider lockProvider,  String lockName) {
+        HazelcastLock lock = lockProvider.getLock(lockName);
+        if (lock != null) {
+            Instant now = now();
+            if (!lock.isExpired(now)) {
+                fail("Expected to be unlocked but got lock " + lock + " current time is " + now);
+            }
+        }
     }
 
     @Test
