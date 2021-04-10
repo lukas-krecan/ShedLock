b90c8f047f021d0e2d059f18e4ee57c19a00fc43
Lukas Krecan <lukas@krecan.net>
2021-04-10 19:53:54 +0200

    Extract AbstractJdbcStorageAccessor

--- providers/jdbc/shedlock-provider-jdbc/src/main/java/net/javacrumbs/shedlock/provider/jdbc/JdbcStorageAccessor.java
+++ providers/jdbc/shedlock-provider-jdbc/src/main/java/net/javacrumbs/shedlock/provider/jdbc/JdbcStorageAccessor.java
@@ -15,87 +15,25 @@
  */
 package net.javacrumbs.shedlock.provider.jdbc;
 
-import net.javacrumbs.shedlock.core.ClockProvider;
-import net.javacrumbs.shedlock.core.LockConfiguration;
-import net.javacrumbs.shedlock.support.AbstractStorageAccessor;
-import net.javacrumbs.shedlock.support.LockException;
 import net.javacrumbs.shedlock.support.annotation.NonNull;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.SQLIntegrityConstraintViolationException;
-import java.sql.Timestamp;
 import java.util.function.BiFunction;
 
 import static java.util.Objects.requireNonNull;
 
-class JdbcStorageAccessor extends AbstractStorageAccessor {
+class JdbcStorageAccessor extends AbstractJdbcStorageAccessor {
     private final DataSource dataSource;
-    private final String tableName;
 
     JdbcStorageAccessor(@NonNull DataSource dataSource, @NonNull String tableName) {
+        super(tableName);
         this.dataSource = requireNonNull(dataSource, "dataSource can not be null");
-        this.tableName = requireNonNull(tableName, "tableName can not be null");
     }
 
-    @Override
-    public boolean insertRecord(@NonNull LockConfiguration lockConfiguration) {
-        // Try to insert if the record does not exists (not optimal, but the simplest platform agnostic way)
-        String sql = "INSERT INTO " + tableName + "(name, lock_until, locked_at, locked_by) VALUES(?, ?, ?, ?)";
-        return executeCommand(sql, statement -> {
-            statement.setString(1, lockConfiguration.getName());
-            statement.setTimestamp(2, Timestamp.from(lockConfiguration.getLockAtMostUntil()));
-            statement.setTimestamp(3, Timestamp.from(ClockProvider.now()));
-            statement.setString(4, getHostname());
-            int insertedRows = statement.executeUpdate();
-            return insertedRows > 0;
-        }, this::handleInsertionException);
-    }
-
-    @Override
-    public boolean updateRecord(@NonNull LockConfiguration lockConfiguration) {
-        String sql = "UPDATE " + tableName + " SET lock_until = ?, locked_at = ?, locked_by = ? WHERE name = ? AND lock_until <= ?";
-        return executeCommand(sql, statement -> {
-            Timestamp now = Timestamp.from(ClockProvider.now());
-            statement.setTimestamp(1, Timestamp.from(lockConfiguration.getLockAtMostUntil()));
-            statement.setTimestamp(2, now);
-            statement.setString(3, getHostname());
-            statement.setString(4, lockConfiguration.getName());
-            statement.setTimestamp(5, now);
-            int updatedRows = statement.executeUpdate();
-            return updatedRows > 0;
-        }, this::handleUpdateException);
-    }
-
-    @Override
-    public boolean extend(@NonNull LockConfiguration lockConfiguration) {
-        String sql = "UPDATE " + tableName + " SET lock_until = ? WHERE name = ? AND locked_by = ? AND lock_until > ? ";
-
-        logger.debug("Extending lock={} until={}", lockConfiguration.getName(), lockConfiguration.getLockAtMostUntil());
-
-        return executeCommand(sql, statement -> {
-            statement.setTimestamp(1, Timestamp.from(lockConfiguration.getLockAtMostUntil()));
-            statement.setString(2, lockConfiguration.getName());
-            statement.setString(3, getHostname());
-            statement.setTimestamp(4, Timestamp.from(ClockProvider.now()));
-            return statement.executeUpdate() > 0;
-        }, this::handleUnlockException);
-    }
-
-    @Override
-    public void unlock(@NonNull LockConfiguration lockConfiguration) {
-        String sql = "UPDATE " + tableName + " SET lock_until = ? WHERE name = ?";
-        executeCommand(sql, statement -> {
-            statement.setTimestamp(1, Timestamp.from(lockConfiguration.getUnlockTime()));
-            statement.setString(2, lockConfiguration.getName());
-            statement.executeUpdate();
-            return null;
-        }, this::handleUnlockException);
-    }
-
-    private <T> T executeCommand(
+    protected <T> T executeCommand(
         String sql,
         SqlFunction<PreparedStatement, T> body,
         BiFunction<String, SQLException, T> exceptionHandler
@@ -109,29 +47,5 @@ class JdbcStorageAccessor extends AbstractStorageAccessor {
         } catch (SQLException e) {
             return exceptionHandler.apply(sql, e);
         }
-    }
-
-    @FunctionalInterface
-    interface SqlFunction<T, R> {
-        R apply(T t) throws SQLException;
-    }
-
-    boolean handleInsertionException(String sql, SQLException e) {
-        if (e instanceof SQLIntegrityConstraintViolationException) {
-            // lock record already exists
-        } else {
-            // can not throw exception here, some drivers (Postgres) do not throw SQLIntegrityConstraintViolationException on duplicate key
-            // we will try update in the next step, su if there is another problem, an exception will be thrown there
-            logger.debug("Exception thrown when inserting record", e);
-        }
-        return false;
-    }
-
-    boolean handleUpdateException(String sql, SQLException e) {
-        throw new LockException("Unexpected exception when locking", e);
-    }
-
-    boolean handleUnlockException(String sql, SQLException e) {
-        throw new LockException("Unexpected exception when unlocking", e);
     }
 }
