08be94718a2876b9676cfc4027a3b7b3c0453416
Mikhael Sokolov <sokomishalov@mail.ru>
2021-10-06 19:24:52 +0300

    r2dbc provider (#737)

---
+++ providers/r2dbc/shedlock-provider-r2dbc/src/main/java/net/javacrumbs/shedlock/provider/r2dbc/R2dbcStorageAccessor.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.javacrumbs.shedlock.provider.r2dbc;
+
+import io.r2dbc.spi.Connection;
+import io.r2dbc.spi.ConnectionFactory;
+import io.r2dbc.spi.Statement;
+import net.javacrumbs.shedlock.provider.r2dbc.internal.AbstractR2dbcStorageAccessor;
+import net.javacrumbs.shedlock.support.annotation.NonNull;
+import reactor.core.publisher.Mono;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+import static java.util.Objects.requireNonNull;
+
+class R2dbcStorageAccessor extends AbstractR2dbcStorageAccessor {
+    private final ConnectionFactory connectionFactory;
+
+    R2dbcStorageAccessor(@NonNull ConnectionFactory connectionFactory, @NonNull String tableName) {
+        super(tableName);
+        this.connectionFactory = requireNonNull(connectionFactory, "dataSource can not be null");
+    }
+
+    @Override
+    protected <T> Mono<T> executeCommand(
+        String sql,
+        Function<Statement, Mono<T>> body,
+        BiFunction<String, Throwable, Mono<T>> exceptionHandler
+    ) {
+        return Mono.usingWhen(
+            Mono.from(connectionFactory.create()).doOnNext(it -> it.setAutoCommit(true)),
+            conn -> body.apply(conn.createStatement(sql)).onErrorResume(throwable -> exceptionHandler.apply(sql, throwable)),
+            Connection::close,
+            (connection, throwable) -> exceptionHandler.apply(sql, throwable),
+            connection -> Mono.empty()
+        );
+    }
+}
