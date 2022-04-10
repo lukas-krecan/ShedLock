9b47605320f4578dbae88007836489ea78a1e573
Lukas Krecan <lukas@krecan.net>
2022-04-10 20:21:32 +0200

    Borken module for shedlock spring

---
+++ spring/shedlock-spring/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module net.javacrumbs.shedlock.spring {
+    requires net.javacrumbs.shedlock.core;
+    requires spring.beans;
+    requires spring.context;
+    requires spring.core;
+    requires spring.aop;
+    requires org.slf4j;
+    opens net.javacrumbs.shedlock.spring.aop to spring.beans, spring.core;
+    exports net.javacrumbs.shedlock.spring.annotation;
+}
---
+++ spring/shedlock-spring/src/test/java/module-info.java
@@ -0,0 +1,13 @@
+open module net.javacrumbs.shedlock.spring.test {
+    requires net.javacrumbs.shedlock.spring;
+    requires org.junit.jupiter.engine;
+    requires org.junit.jupiter.api;
+    requires net.javacrumbs.shedlock.core;
+    requires org.mockito;
+    requires org.slf4j;
+    requires spring.context;
+    requires spring.test;
+    requires spring.beans;
+    requires awaitility;
+    requires org.assertj.core;
+}
