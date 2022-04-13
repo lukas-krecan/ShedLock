15302b8fe5530369c7a001c1950b0498d1d99325
Lukas Krecan <lukas@krecan.net>
2022-04-13 12:30:33 +0200

    Test with more readable time value

--- spring/test/shedlock-springboot-test/src/main/java/net/javacrumbs/shedlock/test/boot/Application.java
+++ spring/test/shedlock-springboot-test/src/main/java/net/javacrumbs/shedlock/test/boot/Application.java
@@ -27,7 +27,7 @@ import javax.sql.DataSource;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableSchedulerLock(defaultLockAtMostFor = "PT10M")
+@EnableSchedulerLock(defaultLockAtMostFor = "10m")
 public class Application {
 
     public static void main(String[] args) {
