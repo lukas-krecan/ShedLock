2939161788e9f2a06f6bf90b2117d1dc0cf20782
Lukas Krecan <lukas@krecan.net>
2022-11-19 07:58:55 +0100

    TX support

--- providers/jdbc/shedlock-provider-jooq/src/main/java/net/javacrumbs/shedlock/provider/jooq/JooqStorageAccessor.java
+++ providers/jdbc/shedlock-provider-jooq/src/main/java/net/javacrumbs/shedlock/provider/jooq/JooqStorageAccessor.java
@@ -22,6 +22,7 @@ import static org.jooq.impl.DSL.when;
 
 class JooqStorageAccessor extends AbstractStorageAccessor {
     private final DSLContext dslContext;
+    private final Shedlock t = SHEDLOCK;
 
     JooqStorageAccessor(DSLContext dslContext) {
         this.dslContext = dslContext;
@@ -29,42 +30,42 @@ class JooqStorageAccessor extends AbstractStorageAccessor {
 
     @Override
     public boolean insertRecord(@NonNull LockConfiguration lockConfiguration) {
-        return dslContext.insertInto(SHEDLOCK)
-            .set(getData(lockConfiguration))
+        return dslContext.transactionResult(tx -> tx.dsl().insertInto(t)
+            .set(data(lockConfiguration))
             .onConflictDoNothing()
-            .execute() > 0;
+            .execute() > 0);
     }
 
     @Override
     public boolean updateRecord(@NonNull LockConfiguration lockConfiguration) {
-        return dslContext.update(SHEDLOCK)
-            .set(getData(lockConfiguration))
-            .where(SHEDLOCK.NAME.eq(lockConfiguration.getName()).and(SHEDLOCK.LOCK_UNTIL.le(now())))
-            .execute() > 0;
+        return dslContext.transactionResult(tx -> tx.dsl().update(t)
+            .set(data(lockConfiguration))
+            .where(t.NAME.eq(lockConfiguration.getName()).and(t.LOCK_UNTIL.le(now())))
+            .execute() > 0);
     }
 
     @Override
     public void unlock(LockConfiguration lockConfiguration) {
-        Field<LocalDateTime> lockAtLeastFor = SHEDLOCK.LOCKED_AT.add(DayToSecond.valueOf(lockConfiguration.getLockAtLeastFor()));
-        dslContext.update(SHEDLOCK).set(SHEDLOCK.LOCK_UNTIL, when(lockAtLeastFor.gt(now()), lockAtLeastFor).otherwise(now()))
-            .where(SHEDLOCK.NAME.eq(lockConfiguration.getName()).and(SHEDLOCK.LOCKED_BY.eq(getHostname())))
-            .execute();
+        Field<LocalDateTime> lockAtLeastFor = t.LOCKED_AT.add(DayToSecond.valueOf(lockConfiguration.getLockAtLeastFor()));
+        dslContext.transaction(tx -> tx.dsl().update(t).set(t.LOCK_UNTIL, when(lockAtLeastFor.gt(now()), lockAtLeastFor).otherwise(now()))
+            .where(t.NAME.eq(lockConfiguration.getName()).and(t.LOCKED_BY.eq(getHostname())))
+            .execute());
     }
 
     @Override
-    public boolean extend(LockConfiguration lockConfiguration) {
-        return dslContext.update(SHEDLOCK).set(SHEDLOCK.LOCK_UNTIL, nowPlus(lockConfiguration.getLockAtMostFor()))
-            .where(SHEDLOCK.NAME.eq(lockConfiguration.getName()).and(SHEDLOCK.LOCKED_BY.eq(getHostname())).and(SHEDLOCK.LOCK_UNTIL.gt(now())))
-            .execute() > 0;
+    public boolean extend(@NonNull LockConfiguration lockConfiguration) {
+        return dslContext.transactionResult(tx -> tx.dsl().update(t).set(t.LOCK_UNTIL, nowPlus(lockConfiguration.getLockAtMostFor()))
+            .where(t.NAME.eq(lockConfiguration.getName()).and(t.LOCKED_BY.eq(getHostname())).and(t.LOCK_UNTIL.gt(now())))
+            .execute() > 0);
     }
 
 
-    private Map<? extends TableField<Record, ? extends Serializable>, Serializable> getData(LockConfiguration lockConfiguration) {
+    private Map<? extends TableField<Record, ? extends Serializable>, Serializable> data(LockConfiguration lockConfiguration) {
         return Map.of(
-            SHEDLOCK.NAME, lockConfiguration.getName(),
-            SHEDLOCK.LOCK_UNTIL, nowPlus(lockConfiguration.getLockAtMostFor()),
-            SHEDLOCK.LOCKED_AT, now(),
-            SHEDLOCK.LOCKED_BY, getHostname()
+            t.NAME, lockConfiguration.getName(),
+            t.LOCK_UNTIL, nowPlus(lockConfiguration.getLockAtMostFor()),
+            t.LOCKED_AT, now(),
+            t.LOCKED_BY, getHostname()
         );
     }
 
