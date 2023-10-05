0d3f486d29d56f8d3a925598c58ea97b7cd55039
C1329393 Ricardo Jorge Luz Rufino <c1329393@interno.bb.com.br>
2023-10-05 14:00:17 -0300

    Create default provider/factory

--- providers/redis/shedlock-provider-redis-quarkus2/src/main/java/net/javacrumbs/shedlock/provider/redis/quarkus2/SchedulerLockFactory.java
+++ providers/redis/shedlock-provider-redis-quarkus2/src/main/java/net/javacrumbs/shedlock/provider/redis/quarkus2/SchedulerLockFactory.java
@@ -1,24 +1,25 @@
-package net.javacrumbs.shedlock.provider.redis.quarkus;
+package net.javacrumbs.shedlock.provider.redis.quarkus2;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.inject.Alternative;
 import javax.enterprise.inject.Produces;
 import javax.inject.Singleton;
 
-import io.quarkus.arc.Priority;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import io.quarkus.redis.datasource.RedisDataSource;
 import net.javacrumbs.shedlock.core.LockProvider;
-import net.javacrumbs.shedlock.provider.redis.quarkus2.QuarkusRedisLockProvider;
 
 
 @ApplicationScoped
-public class ScheduleLockProvider {
+public class SchedulerLockFactory {
+    
+    @ConfigProperty(name = "quarkus.application.name")
+    String app;
     
     @Produces
     @Singleton
-    @Alternative()
-    @Priority(1)
     public LockProvider lockProvider(RedisDataSource redisDataSource) {
-        return new QuarkusRedisLockProvider(redisDataSource, "my-app");
+        
+        return new QuarkusRedisLockProvider(redisDataSource, app);
     }
 }
