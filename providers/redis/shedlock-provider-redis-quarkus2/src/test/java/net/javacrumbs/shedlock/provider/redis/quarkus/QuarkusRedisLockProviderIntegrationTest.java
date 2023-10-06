d4fdf5cb29316f7637edbc72dc2ae7a9dd361b44
C1329393 Ricardo Jorge Luz Rufino <c1329393@interno.bb.com.br>
2023-10-05 21:25:57 -0300

    tests using AbstractExtensibleLockProviderIntegrationTest

--- providers/redis/shedlock-provider-redis-quarkus2/src/test/java/net/javacrumbs/shedlock/provider/redis/quarkus/QuarkusRedisLockProviderIntegrationTest.java
+++ providers/redis/shedlock-provider-redis-quarkus2/src/test/java/net/javacrumbs/shedlock/provider/redis/quarkus/QuarkusRedisLockProviderIntegrationTest.java
@@ -1,138 +1,59 @@
 package net.javacrumbs.shedlock.provider.redis.quarkus;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import javax.inject.Inject;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
 
 import io.quarkus.redis.datasource.RedisDataSource;
+import io.quarkus.redis.datasource.value.ValueCommands;
 import io.quarkus.test.junit.QuarkusTest;
+import net.javacrumbs.shedlock.core.ExtensibleLockProvider;
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.test.support.AbstractExtensibleLockProviderIntegrationTest;
+
 
 @QuarkusTest
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class QuarkusRedisLockProviderIntegrationTest {
-    
+public class QuarkusRedisLockProviderIntegrationTest extends AbstractExtensibleLockProviderIntegrationTest {
+
     @Inject
-    LockedService lockedService;
-    
+    LockProvider lockProvider;
+
     @Inject
     RedisDataSource dataSource;
-    
-    private ExecutorService executorService = Executors.newFixedThreadPool(5);
-    
-    @AfterEach
-    public void afterEach() {
-        lockedService.countReset();
-        
-        // delete keys
-        List<String> keys = dataSource.key().keys("*");
-        for (String key : keys) {
-            dataSource.key().del(key);
-        }
-        
-        try {
-            executorService.awaitTermination(5, TimeUnit.SECONDS);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-    
-    
 
-    @Test
-    @Order(1)
-    void test_warmUp() throws Exception {
-        lockedService.test(100);
-        assertEquals(1, lockedService.count());
+    private ValueCommands<String, String> values;
+    
+    @BeforeEach
+    public void beforeEach() {
+        this.values = dataSource.value(String.class);
     }
     
     @Test
-    void test_sequenceCalls() throws Exception {
-        lockedService.test(50);
-        lockedService.test(50);
-        assertEquals(2, lockedService.count());
+    void warmUp() throws Exception {
+        this.values.getDataSource();
     }
     
-    @Test
-    @Order(2)
-    void test_basicLock() throws Exception {
-        
-        executorService.execute(() -> { lockedService.test(300); });
-        Thread.sleep(100);
-        assertTrue(isLockExist("test"), this::lockMessage);
-        
-        executorService.execute(() -> { lockedService.test(300); }); // skip this...
-        Thread.sleep(300);
-        
-        assertFalse(isLockExist("test"), this::lockMessage);
-        assertEquals(1, lockedService.count());
-        
-        
-    }
-    
-
-    @Test
-    void test_highConcurrency() throws Exception {
-        
-        for (int i = 0; i < 100; i++) {
-            executorService.execute(() -> { lockedService.test(2000); });
-            Thread.sleep(10);
-        }
-        
-        assertTrue(isLockExist("test"), this::lockMessage);
-        assertEquals(1, lockedService.count());
-    }
-    
-    
-    @Test
-    void test_lockFinished() throws Exception {
-        
-        executorService.execute(() -> { lockedService.test(100); });
-        Thread.sleep(200);
-        
-        executorService.execute(() -> { lockedService.test(100); });
-        Thread.sleep(200);
-        
-        assertEquals(2, lockedService.count());
-        
-    }
-    
-    @Test
-    void test_ReleaseLock_onException() throws Exception {
-        
-        executorService.execute(() -> { lockedService.testException(); });
-        Thread.sleep(100);
-        
-        assertTrue(isLockExist("testException")); // not fired exception
-        
-        // validate Lock (after exception fired)
-        Thread.sleep(500);
-        assertFalse(isLockExist("testException"));
-        
-    }
-    
-    private boolean isLockExist(String name) {
-        
-        return dataSource.key().exists("lock:my-app:test:" + name);
-        
-    }
-    
-    private String lockMessage() {
-        return "Current Keys: " + dataSource.key().keys("*");
+    @Override
+    protected void assertUnlocked(String lockName) {
+        assertThat(getLock(lockName)).isNull();
     }
 
-    
+    @Override
+    protected void assertLocked(String lockName) {
+        assertThat(getLock(lockName)).isNotNull();
+    }
+
+    private String getLock(String lockName) {
+        return values.get("lock:my-app:test:"+lockName);
+    }
+
+    @Override
+    protected ExtensibleLockProvider getLockProvider() {
+        return (ExtensibleLockProvider) lockProvider;
+    }
+
 
 }
