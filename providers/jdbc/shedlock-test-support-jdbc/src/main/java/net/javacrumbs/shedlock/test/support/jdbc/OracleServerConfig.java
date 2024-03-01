05eaca2121fd55c37c783fa30e9ba87da1dcb241
Lukas Krecan <lukas@krecan.net>
2024-03-01 15:40:14 +0100

    Upgrade test Oracle version

--- providers/jdbc/shedlock-test-support-jdbc/src/main/java/net/javacrumbs/shedlock/test/support/jdbc/OracleServerConfig.java
+++ providers/jdbc/shedlock-test-support-jdbc/src/main/java/net/javacrumbs/shedlock/test/support/jdbc/OracleServerConfig.java
@@ -19,7 +19,7 @@ import org.testcontainers.containers.OracleContainer;
 
 public final class OracleServerConfig extends AbstractContainerBasedDbConfig<OracleContainer> {
     public OracleServerConfig() {
-        super(new OracleContainer("gvenzl/oracle-xe:18-slim"));
+        super(new OracleContainer("gvenzl/oracle-xe:21-slim"));
     }
 
     @Override
