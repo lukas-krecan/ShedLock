0c44a56b4f8d176e90a99000a5087619acc4d6cc
Lukas Krecan <lukas@krecan.net>
2024-08-25 20:34:40 +0200

    Get rid of fongo

--- providers/mongo/shedlock-provider-mongo-reactivestreams/src/test/java/net/javacrumbs/shedlock/provider/mongo/reactivestreams/ReactiveStreamsMongoLockProviderIntegrationTest.java
+++ providers/mongo/shedlock-provider-mongo-reactivestreams/src/test/java/net/javacrumbs/shedlock/provider/mongo/reactivestreams/ReactiveStreamsMongoLockProviderIntegrationTest.java
@@ -27,12 +27,6 @@ import com.mongodb.client.result.DeleteResult;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
 import com.mongodb.reactivestreams.client.MongoCollection;
-import de.flapdoodle.embed.mongo.MongodExecutable;
-import de.flapdoodle.embed.mongo.MongodProcess;
-import de.flapdoodle.embed.mongo.MongodStarter;
-import de.flapdoodle.embed.mongo.config.MongodConfig;
-import de.flapdoodle.embed.mongo.distribution.Version;
-import java.io.IOException;
 import java.util.Date;
 import net.javacrumbs.shedlock.core.ExtensibleLockProvider;
 import net.javacrumbs.shedlock.core.LockProvider;
@@ -42,32 +36,27 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 public class ReactiveStreamsMongoLockProviderIntegrationTest extends AbstractExtensibleLockProviderIntegrationTest {
-    private static final MongodStarter starter = MongodStarter.getDefaultInstance();
-
     private static final String DB_NAME = "db";
 
-    private static MongodExecutable mongodExe;
-    private static MongodProcess mongod;
+    @Container
+    public static final MongoDBContainer container = new MongoDBContainer("mongo:4.0.10");
 
     private static MongoClient mongo;
 
     @BeforeAll
-    public static void startMongo() throws IOException {
-        mongodExe = starter.prepare(
-                MongodConfig.builder().version(Version.Main.V3_6).build());
-        mongod = mongodExe.start();
-
-        mongo = MongoClients.create(
-                "mongodb://localhost:" + mongod.getConfig().net().getPort());
+    public static void startMongo() {
+        mongo = MongoClients.create("mongodb://" + container.getHost() + ":" + container.getFirstMappedPort());
     }
 
     @AfterAll
     public static void stopMongo() {
         mongo.close();
-        mongod.stop();
-        mongodExe.stop();
     }
 
     @BeforeEach
@@ -83,8 +72,8 @@ public class ReactiveStreamsMongoLockProviderIntegrationTest extends AbstractExt
     @Override
     protected void assertUnlocked(String lockName) {
         Document lockDocument = getLockDocument(lockName);
-        assertThat((Date) lockDocument.get(LOCK_UNTIL)).isBeforeOrEqualsTo(now());
-        assertThat((Date) lockDocument.get(LOCKED_AT)).isBeforeOrEqualsTo(now());
+        assertThat((Date) lockDocument.get(LOCK_UNTIL)).isBeforeOrEqualTo(now());
+        assertThat((Date) lockDocument.get(LOCKED_AT)).isBeforeOrEqualTo(now());
         assertThat((String) lockDocument.get(LOCKED_BY)).isNotEmpty();
     }
 
@@ -96,7 +85,7 @@ public class ReactiveStreamsMongoLockProviderIntegrationTest extends AbstractExt
     protected void assertLocked(String lockName) {
         Document lockDocument = getLockDocument(lockName);
         assertThat((Date) lockDocument.get(LOCK_UNTIL)).isAfter(now());
-        assertThat((Date) lockDocument.get(LOCKED_AT)).isBeforeOrEqualsTo(now());
+        assertThat((Date) lockDocument.get(LOCKED_AT)).isBeforeOrEqualTo(now());
         assertThat((String) lockDocument.get(LOCKED_BY)).isNotEmpty();
     }
 
