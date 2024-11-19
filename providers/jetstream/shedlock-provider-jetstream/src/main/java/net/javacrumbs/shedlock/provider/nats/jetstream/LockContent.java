163ef48890e33421a9012f0f6c4dfda135197da1
Martin Nyholm Jelle <mjelle@hotmail.com>
2024-11-19 22:38:04 +0100

    Inspired from Consul Provider is handling of lockAtLeastFor

--- providers/jetstream/shedlock-provider-jetstream/src/main/java/net/javacrumbs/shedlock/provider/nats/jetstream/LockContent.java
+++
@@ -1,6 +0,0 @@
-package net.javacrumbs.shedlock.provider.nats.jetstream;
-
-import java.time.Instant;
-import net.javacrumbs.shedlock.support.annotation.NonNull;
-
-public record LockContent(@NonNull Instant lockAtLeastUntil, @NonNull Instant lockAtMostUntil) {}
--- providers/jetstream/shedlock-provider-jetstream/src/main/java/net/javacrumbs/shedlock/provider/nats/jetstream/NatsJetStreamLockProvider.java
+++ providers/jetstream/shedlock-provider-jetstream/src/main/java/net/javacrumbs/shedlock/provider/nats/jetstream/NatsJetStreamLockProvider.java
@@ -3,12 +3,20 @@ package net.javacrumbs.shedlock.provider.nats.jetstream;
 import io.nats.client.Connection;
 import io.nats.client.JetStreamApiException;
 import io.nats.client.api.KeyValueConfiguration;
+
+import static net.javacrumbs.shedlock.core.ClockProvider.now;
+
 import java.io.IOException;
 import java.time.Duration;
 import java.util.Optional;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 import net.javacrumbs.shedlock.core.LockConfiguration;
 import net.javacrumbs.shedlock.core.LockProvider;
 import net.javacrumbs.shedlock.core.SimpleLock;
+import net.javacrumbs.shedlock.support.LockException;
 import net.javacrumbs.shedlock.support.annotation.NonNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,13 +26,13 @@ import org.slf4j.LoggerFactory;
  *
  * @see <a href="https://docs.nats.io/nats-concepts/jetstream">JetStream</a>
  */
-public class NatsJetStreamLockProvider implements LockProvider {
+public class NatsJetStreamLockProvider implements LockProvider, AutoCloseable {
 
     private final Logger log = LoggerFactory.getLogger(NatsJetStreamLockProvider.class);
+    
+    private final ScheduledExecutorService unlockScheduler = Executors.newSingleThreadScheduledExecutor();
 
     private final Connection connection;
-
-    private boolean issueLockAtLeastWarning = true;
 
     /**
      * Create NatsJetStreamLockProvider
@@ -42,14 +50,10 @@ public class NatsJetStreamLockProvider implements LockProvider {
         var bucketName = String.format("SHEDLOCK-%s", lockConfiguration.getName());
         log.debug("Attempting lock for bucketName: {}", bucketName);
         try {
-            if (!lockConfiguration.getLockAtLeastFor().isZero() && issueLockAtLeastWarning) {
-                log.warn("lockAtLeastFor does work special for NatsJetStreamLockProvider. Read documentation!");
-                issueLockAtLeastWarning = false;
-            }
             var lockTime = lockConfiguration.getLockAtMostFor();
 
             // nats cannot accept below 100ms
-            if (lockTime.toMillis() < 100) {
+            if (lockTime.toMillis() < 100L) {
                 log.debug(
                         "NATS must be above 100ms for smallest locktime, correcting {}ms to 100ms!",
                         lockTime.toMillis());
@@ -66,12 +70,11 @@ public class NatsJetStreamLockProvider implements LockProvider {
                     .keyValue(bucketName)
                     .create(
                             "LOCKED",
-                            LockContentHandler.writeContent(new LockContent(
-                                    lockConfiguration.getLockAtLeastUntil(), lockConfiguration.getLockAtMostUntil())));
+                            "ShedLock internal value. Do not touch.".getBytes());
 
             log.debug("Acquired lock for bucketName: {}", bucketName);
 
-            return Optional.of(new NatsJetStreamLock(connection, lockConfiguration));
+            return Optional.of(new NatsJetStreamLock(lockConfiguration, this));
         } catch (JetStreamApiException e) {
             if (e.getApiErrorCode() == 10071) {
                 log.debug("Rejected lock for bucketName: {}, message: {}", bucketName, e.getMessage());
@@ -85,6 +88,54 @@ public class NatsJetStreamLockProvider implements LockProvider {
             throw new IllegalStateException(e);
         } catch (IOException e) {
             throw new IllegalStateException(e);
+        }
+    }
+
+    void unlock(LockConfiguration lockConfiguration) {
+        var bucketName = String.format("SHEDLOCK-%s", lockConfiguration.getName());
+        log.debug("Unlocking for bucketName: {}", bucketName);
+        var additionalSessionTtl = Duration.between(now(), lockConfiguration.getLockAtLeastUntil());
+        if (!additionalSessionTtl.isNegative() && !additionalSessionTtl.isZero()) {
+            log.debug("Lock will still be held for {}", additionalSessionTtl);
+            scheduleUnlock(bucketName, additionalSessionTtl);
+        } else {
+            destroy(bucketName);
+        }
+    }
+
+    private void scheduleUnlock(String bucketName, Duration unlockTime) {
+        unlockScheduler.schedule(
+                catchExceptions(() -> destroy(bucketName)), unlockTime.toMillis(), TimeUnit.MILLISECONDS);
+    }
+
+    private void destroy(String bucketName) {
+        log.debug("Destroying key in bucketName: {}", bucketName);
+        try {
+            connection.keyValue(bucketName).delete("LOCKED");
+        } catch (Exception e) {
+            throw new LockException("Can not remove key. " + e.getMessage());
+        }
+    }
+
+    private Runnable catchExceptions(Runnable runnable) {
+        return () -> {
+            try {
+                runnable.run();
+            } catch (Throwable t) {
+                log.warn("Exception while execution", t);
+            }
+        };
+    }
+
+    @Override
+    public void close() {
+        unlockScheduler.shutdown();
+        try {
+            if (!unlockScheduler.awaitTermination(
+                Duration.ofSeconds(2).toMillis(), TimeUnit.MILLISECONDS)) {
+                unlockScheduler.shutdownNow();
+            }
+        } catch (InterruptedException ignored) {
         }
     }
 }
