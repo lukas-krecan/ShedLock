d6e09031673e3d0b5afe19b99a4ced6447620408
Lukas Krecan <lukas@krecan.net>
2024-11-25 20:08:36 +0100

    MockMvc test

---
+++ spring/test/shedlock-springboot-test/src/test/java/net/javacrumbs/shedlock/test/boot/HelloControllerTest.java
@@ -0,0 +1,21 @@
+package net.javacrumbs.shedlock.test.boot;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(HelloController.class)
+class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldCallController() throws Exception {
+        mockMvc.perform(get("/")).andExpect(status().isOk());
+    }
+}
