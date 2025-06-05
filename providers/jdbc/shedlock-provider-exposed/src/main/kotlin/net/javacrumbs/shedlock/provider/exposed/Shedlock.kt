8139f9bbcbe9f11f69d131a0ff63b1385ad36dad
hungrytech <xorals9448@gmail.com>
2025-06-06 06:38:48 +0900

    Refactor change shedlock internal

--- providers/jdbc/shedlock-provider-exposed/src/main/kotlin/net/javacrumbs/shedlock/provider/exposed/Shedlock.kt
+++ providers/jdbc/shedlock-provider-exposed/src/main/kotlin/net/javacrumbs/shedlock/provider/exposed/Shedlock.kt
@@ -3,7 +3,7 @@ package net.javacrumbs.shedlock.provider.exposed
 import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.javatime.datetime
 
-object Shedlock : Table() {
+internal object Shedlock : Table() {
     override val tableName: String = "shedlock"
     val name = varchar("name", 64)
     val lockUntil = datetime("lock_until")
