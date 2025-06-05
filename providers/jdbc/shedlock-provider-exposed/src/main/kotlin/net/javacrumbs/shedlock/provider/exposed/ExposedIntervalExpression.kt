3c0fc84757f8c84221325d950ec2b62a94916f0d
hungrytech <xorals9448@gmail.com>
2025-06-05 23:54:07 +0900

    Add ms SQL test

--- providers/jdbc/shedlock-provider-exposed/src/main/kotlin/net/javacrumbs/shedlock/provider/exposed/ExposedIntervalExpression.kt
+++ providers/jdbc/shedlock-provider-exposed/src/main/kotlin/net/javacrumbs/shedlock/provider/exposed/ExposedIntervalExpression.kt
@@ -2,22 +2,37 @@ package net.javacrumbs.shedlock.provider.exposed
 
 import org.jetbrains.exposed.sql.Expression
 import org.jetbrains.exposed.sql.QueryBuilder
+import org.jetbrains.exposed.sql.vendors.SQLServerDialect
+import org.jetbrains.exposed.sql.vendors.currentDialect
 import java.time.Duration
 import java.time.LocalDateTime
 
 internal fun Expression<LocalDateTime>.plus(duration: Duration): Expression<LocalDateTime> =
-        IntervalExpression(this, duration.seconds)
+        IntervalExpression(this, duration)
 
 private class IntervalExpression(
         private val expression: Expression<LocalDateTime>,
-        private val seconds: Long
+        private val duration: Duration
 ) : Expression<LocalDateTime>() {
     override fun toQueryBuilder(queryBuilder: QueryBuilder) {
-        queryBuilder.let {
-            it.append("(")
-            expression.toQueryBuilder(queryBuilder)
-            it.append(" + INTERVAL '${seconds}' SECOND")
-            it.append(")")
+        when(currentDialect) {
+            is SQLServerDialect -> {
+                queryBuilder.let {
+                    it.append("(DATEADD(millisecond, ")
+                    it.append(duration.toMillis().toString())
+                    it.append(", ")
+                    expression.toQueryBuilder(queryBuilder)
+                    it.append("))")
+                }
+            }
+            else -> {
+                queryBuilder.let {
+                    it.append("(")
+                    expression.toQueryBuilder(queryBuilder)
+                    it.append(" + INTERVAL '${duration.seconds}' SECOND")
+                    it.append(")")
+                }
+            }
         }
     }
 }
