118cdb0ef4f2c9bf17f559c3016b98d1dc4cfcb0
Lukas Krecan <lukas@krecan.net>
2025-06-06 18:53:08 +0200

    Better exception handling in Exposed

--- providers/jdbc/shedlock-provider-exposed/src/main/kotlin/net/javacrumbs/shedlock/provider/exposed/ExposedStorageAccessor.kt
+++ providers/jdbc/shedlock-provider-exposed/src/main/kotlin/net/javacrumbs/shedlock/provider/exposed/ExposedStorageAccessor.kt
@@ -1,9 +1,8 @@
 package net.javacrumbs.shedlock.provider.exposed
 
-import java.time.Duration
-import java.time.LocalDateTime
 import net.javacrumbs.shedlock.core.LockConfiguration
 import net.javacrumbs.shedlock.support.AbstractStorageAccessor
+import net.javacrumbs.shedlock.support.LockException
 import org.jetbrains.exposed.sql.Case
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.Expression
@@ -13,6 +12,11 @@ import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.javatime.CurrentDateTime
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.jetbrains.exposed.sql.update
+import java.sql.SQLException
+import java.sql.SQLIntegrityConstraintViolationException
+import java.time.Duration
+import java.time.LocalDateTime
+
 
 internal class ExposedStorageAccessor(private val database: Database) : AbstractStorageAccessor() {
 
@@ -28,41 +32,55 @@ internal class ExposedStorageAccessor(private val database: Database) : Abstract
                     it[lockedBy] = hostname
                 }
                 true
-            } catch (e: Exception) {
+            } catch (_: SQLIntegrityConstraintViolationException) {
+                // lock record already exists
+                false
+            } catch (e: SQLException) {
                 logger.debug("Exception thrown when inserting record", e)
-
                 false
             }
         }
 
     override fun updateRecord(lockConfiguration: LockConfiguration): Boolean =
         transaction(database) {
-            Shedlock.update({ (Shedlock.name eq lockConfiguration.name) and (Shedlock.lockUntil lessEq now) }) {
-                it[lockUntil] = nowPlus(lockConfiguration.lockAtMostFor)
-                it[lockedAt] = now
-                it[lockedBy] = hostname
-            } > 0
+            try {
+                Shedlock.update({ (Shedlock.name eq lockConfiguration.name) and (Shedlock.lockUntil lessEq now) }) {
+                    it[lockUntil] = nowPlus(lockConfiguration.lockAtMostFor)
+                    it[lockedAt] = now
+                    it[lockedBy] = hostname
+                } > 0
+            } catch (e: SQLException) {
+                throw LockException("Unexpected exception when locking", e)
+            }
         }
 
     override fun unlock(lockConfiguration: LockConfiguration) {
         transaction(database) {
-            val lockAtLeastUntil = Shedlock.lockedAt.plus(lockConfiguration.lockAtLeastFor)
+            try {
+                val lockAtLeastUntil = Shedlock.lockedAt.plus(lockConfiguration.lockAtLeastFor)
 
-            Shedlock.update({ (Shedlock.name eq lockConfiguration.name) and (Shedlock.lockedBy eq hostname) }) {
-                it[lockUntil] = Case().When(lockAtLeastUntil greater now, lockAtLeastUntil).Else(now)
+                Shedlock.update({ (Shedlock.name eq lockConfiguration.name) and (Shedlock.lockedBy eq hostname) }) {
+                    it[lockUntil] = Case().When(lockAtLeastUntil greater now, lockAtLeastUntil).Else(now)
+                }
+            } catch (e: SQLException) {
+                throw LockException("Unexpected exception when unlocking", e)
             }
         }
     }
 
     override fun extend(lockConfiguration: LockConfiguration): Boolean =
         transaction(database) {
-            Shedlock.update({
-                (Shedlock.name eq lockConfiguration.name) and
-                    (Shedlock.lockedBy eq hostname) and
-                    (Shedlock.lockUntil greater now)
-            }) {
-                it[lockUntil] = nowPlus(lockConfiguration.lockAtMostFor)
-            } > 0
+            try {
+                Shedlock.update({
+                    (Shedlock.name eq lockConfiguration.name) and
+                            (Shedlock.lockedBy eq hostname) and
+                            (Shedlock.lockUntil greater now)
+                }) {
+                    it[lockUntil] = nowPlus(lockConfiguration.lockAtMostFor)
+                } > 0
+            } catch (e: SQLException) {
+                throw LockException("Unexpected exception when extending", e)
+            }
         }
 
     private fun nowPlus(duration: Duration): Expression<LocalDateTime> = now.plus(duration)
