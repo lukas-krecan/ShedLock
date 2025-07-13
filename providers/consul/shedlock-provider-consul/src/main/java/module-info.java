64cdf6d167be6885dead97be18e50dcdec33d756
Lukas Krecan <lukas@krecan.net>
2025-07-13 19:51:21 +0200

    JPMS modules

---
+++ providers/consul/shedlock-provider-consul/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+/**
+ * ShedLock Consul provider module.
+ *
+ * This module provides HashiCorp Consul-based lock provider implementation
+ * using the Consul Java client.
+ */
+module net.javacrumbs.shedlock.provider.consul {
+    requires java.base;
+    requires net.javacrumbs.shedlock.core;
+    requires consul.api;
+
+    // Export provider packages
+    exports net.javacrumbs.shedlock.provider.consul;
+}
---
+++ providers/dynamodb/shedlock-provider-dynamodb2/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+/**
+ * ShedLock DynamoDB provider module.
+ *
+ * This module provides Amazon DynamoDB-based lock provider implementation
+ * using AWS SDK v2.
+ */
+module net.javacrumbs.shedlock.provider.dynamodb2 {
+    requires java.base;
+    requires net.javacrumbs.shedlock.core;
+    requires software.amazon.awssdk.services.dynamodb;
+    requires software.amazon.awssdk.core;
+    requires software.amazon.awssdk.utils;
+
+    // Export provider packages
+    exports net.javacrumbs.shedlock.provider.dynamodb2;
+}
