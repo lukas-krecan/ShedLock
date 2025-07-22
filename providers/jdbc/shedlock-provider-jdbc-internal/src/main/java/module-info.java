210de209d16fe00bc0ba6245ba947e3d835c8371
Lukas Krecan <lukas@krecan.net>
2025-07-22 09:38:02 +0200

    Fix JDBC 2

--- providers/jdbc/shedlock-provider-jdbc-internal/src/main/java/module-info.java
+++ providers/jdbc/shedlock-provider-jdbc-internal/src/main/java/module-info.java
@@ -15,8 +15,5 @@ module net.javacrumbs.shedlock.provider.jdbc.internal {
 
     // Export provider packages only to other ShedLock modules
     exports net.javacrumbs.shedlock.provider.jdbc.internal to
-            net.javacrumbs.shedlock.provider.jdbc,
-            net.javacrumbs.shedlock.provider.jdbctemplate,
-            net.javacrumbs.shedlock.provider.jooq,
-            net.javacrumbs.shedlock.provider.r2dbc;
+            net.javacrumbs.shedlock.provider.jdbc;
 }
