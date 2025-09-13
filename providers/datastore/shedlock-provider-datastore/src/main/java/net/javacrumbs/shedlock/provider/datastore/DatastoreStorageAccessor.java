df0dd8f91bcedda56d1cc72a4d884cd85207b6dc
Lukas Krecan <lukas@krecan.net>
2025-09-13 21:53:43 +0200

    Accessor classes made package protected

--- providers/datastore/shedlock-provider-datastore/src/main/java/net/javacrumbs/shedlock/provider/datastore/DatastoreStorageAccessor.java
+++ providers/datastore/shedlock-provider-datastore/src/main/java/net/javacrumbs/shedlock/provider/datastore/DatastoreStorageAccessor.java
@@ -21,7 +21,7 @@ import org.jspecify.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class DatastoreStorageAccessor extends AbstractStorageAccessor {
+class DatastoreStorageAccessor extends AbstractStorageAccessor {
     private static final Logger log = LoggerFactory.getLogger(DatastoreStorageAccessor.class);
 
     private final Datastore datastore;
