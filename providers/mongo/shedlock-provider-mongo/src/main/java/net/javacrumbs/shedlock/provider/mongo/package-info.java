749593d86b22580540c323e5862a2196767fcd48
Lukas Krecan <lukas@krecan.net>
2025-09-13 20:16:07 +0200

    Migrate to JSpecify

--- providers/mongo/shedlock-provider-mongo/src/main/java/net/javacrumbs/shedlock/provider/mongo/package-info.java
+++ providers/mongo/shedlock-provider-mongo/src/main/java/net/javacrumbs/shedlock/provider/mongo/package-info.java
@@ -1,6 +1,2 @@
-@NonNullApi
-@NonNullFields
+@org.jspecify.annotations.NullMarked
 package net.javacrumbs.shedlock.provider.mongo;
-
-import net.javacrumbs.shedlock.support.annotation.NonNullApi;
-import net.javacrumbs.shedlock.support.annotation.NonNullFields;
