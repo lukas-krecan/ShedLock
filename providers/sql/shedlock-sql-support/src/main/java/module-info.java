ded36f41ecc4ceabdbc9d1b94bd6b2eaff855991
Lukas Krecan <lukas@krecan.net>
2025-09-24 14:48:57 +0200

    Keep SQL  module internal in module-info.java

--- providers/sql/shedlock-sql-support/src/main/java/module-info.java
+++ providers/sql/shedlock-sql-support/src/main/java/module-info.java
@@ -1,5 +1,9 @@
 module net.javacrumbs.shedlock.provider.sql {
     requires net.javacrumbs.shedlock.core;
     // Export provider packages
-    exports net.javacrumbs.shedlock.provider.sql;
+    exports net.javacrumbs.shedlock.provider.sql to
+            net.javacrumbs.shedlock.provider.jdbc.internal,
+            net.javacrumbs.shedlock.provider.jdbc,
+            net.javacrumbs.shedlock.provider.jdbctemplate,
+            net.javacrumbs.shedlock.provider.jdbc.micronaut;
 }
