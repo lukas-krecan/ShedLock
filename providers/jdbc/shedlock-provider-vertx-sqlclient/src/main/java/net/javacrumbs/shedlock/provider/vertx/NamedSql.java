de2c5afe030c891a01525db15706af3854d552ca
Lukas Krecan <lukas@krecan.net>
2025-09-25 17:09:29 +0200

    Client side Vert.x works on Postgres

--- providers/jdbc/shedlock-provider-vertx-sqlclient/src/main/java/net/javacrumbs/shedlock/provider/vertx/NamedSql.java
+++ providers/jdbc/shedlock-provider-vertx-sqlclient/src/main/java/net/javacrumbs/shedlock/provider/vertx/NamedSql.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.Pattern;
 
 class NamedSql {
@@ -11,13 +12,14 @@ class NamedSql {
 
     static Statement translate(String namedSql, Map<String, Object> namedParameters) {
         List<Object> parameters = new ArrayList<>();
+        AtomicInteger counter = new AtomicInteger(0);
         String sql = NAMED_PARAMETER_PATTERN.matcher(namedSql).replaceAll(result -> {
             String key = result.group().substring(1);
             if (!namedParameters.containsKey(key)) {
                 throw new IllegalStateException("Parameter " + key + " not found");
             }
             parameters.add(namedParameters.get(key));
-            return "?";
+            return "\\$" + counter.incrementAndGet();
         });
         return new Statement(sql, Collections.unmodifiableList(parameters));
     }
