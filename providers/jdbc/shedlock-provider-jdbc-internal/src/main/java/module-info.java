6c3205bb4ee5eb217a1bfe8da9d78d8435f3d167
Lukas Krecan <lukas@krecan.net>
2025-10-07 16:45:16 +0200

    Error prone

--- providers/jdbc/shedlock-provider-jdbc-internal/src/main/java/module-info.java
+++ providers/jdbc/shedlock-provider-jdbc-internal/src/main/java/module-info.java
@@ -14,8 +14,6 @@ module net.javacrumbs.shedlock.provider.jdbc.internal {
     // Allow JDBC driver service loading
     uses java.sql.Driver;
 
-    // Export provider packages only to other ShedLock modules
-    exports net.javacrumbs.shedlock.provider.jdbc.internal to
-            net.javacrumbs.shedlock.provider.jdbc,
-            net.javacrumbs.shedlock.provider.jdbc.micronaut;
+    // Export provider package
+    exports net.javacrumbs.shedlock.provider.jdbc.internal;
 }
