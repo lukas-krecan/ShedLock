cde5c2c2f7f827a8bdd7287848197f8f75bb8848
Lukas Krecan <lukas@krecan.net>
2025-10-11 20:56:49 +0200

    More precise SupressWarnings

--- providers/jdbc/shedlock-test-support-jdbc/src/main/java/net/javacrumbs/shedlock/test/support/jdbc/Db2ServerConfig.java
+++ providers/jdbc/shedlock-test-support-jdbc/src/main/java/net/javacrumbs/shedlock/test/support/jdbc/Db2ServerConfig.java
@@ -20,7 +20,7 @@ import org.testcontainers.utility.DockerImageName;
 
 public final class Db2ServerConfig extends AbstractDbConfig {
 
-    @SuppressWarnings("NullAway")
+    @SuppressWarnings("NullAway.Init")
     private Db2Container db2;
 
     private static final Logger logger = LoggerFactory.getLogger(Db2ServerConfig.class);
