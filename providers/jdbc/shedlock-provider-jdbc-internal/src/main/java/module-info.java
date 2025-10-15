6334d4baa9310afa1f0bc86b84610e6bd62e2050
Lukas Krecan <lukas@krecan.net>
2025-10-15 15:01:32 +0200

    Users need to use classes from net.javacrumbs.shedlock.provider.sql

--- providers/jdbc/shedlock-provider-jdbc-internal/src/main/java/module-info.java
+++ providers/jdbc/shedlock-provider-jdbc-internal/src/main/java/module-info.java
@@ -7,7 +7,7 @@
 module net.javacrumbs.shedlock.provider.jdbc.internal {
     requires java.sql;
     requires net.javacrumbs.shedlock.core;
-    requires net.javacrumbs.shedlock.provider.sql;
+    requires transitive net.javacrumbs.shedlock.provider.sql;
     requires static org.jspecify;
     requires org.slf4j;
 
--- providers/sql/shedlock-sql-support/src/main/java/module-info.java
+++ providers/sql/shedlock-sql-support/src/main/java/module-info.java
@@ -4,9 +4,5 @@ module net.javacrumbs.shedlock.provider.sql {
     requires static org.jspecify;
     requires org.slf4j;
     // Export provider packages
-    exports net.javacrumbs.shedlock.provider.sql to
-            net.javacrumbs.shedlock.provider.jdbc.internal,
-            net.javacrumbs.shedlock.provider.jdbc,
-            net.javacrumbs.shedlock.provider.jdbctemplate,
-            net.javacrumbs.shedlock.provider.jdbc.micronaut;
+    exports net.javacrumbs.shedlock.provider.sql;
 }
