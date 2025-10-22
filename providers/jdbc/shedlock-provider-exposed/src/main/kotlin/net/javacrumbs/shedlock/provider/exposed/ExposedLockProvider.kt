7d4795667471f167400faec2c618d0ab58a11ffb
Lukas Krecan <lukas@krecan.net>
2025-10-22 14:52:55 +0200

    Upgrade Exposed to 1.0

--- providers/jdbc/shedlock-provider-exposed/src/main/kotlin/net/javacrumbs/shedlock/provider/exposed/ExposedLockProvider.kt
+++ providers/jdbc/shedlock-provider-exposed/src/main/kotlin/net/javacrumbs/shedlock/provider/exposed/ExposedLockProvider.kt
@@ -1,6 +1,6 @@
 package net.javacrumbs.shedlock.provider.exposed
 
 import net.javacrumbs.shedlock.support.StorageBasedLockProvider
-import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.v1.jdbc.Database
 
 class ExposedLockProvider(database: Database) : StorageBasedLockProvider(ExposedStorageAccessor(database))
