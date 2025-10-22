be7fdf0455c819196e0d2322a3d4dbd3c38f3fc5
Lukas Krecan <lukas@krecan.net>
2025-10-22 12:52:53 +0200

    Exposed throws LockException in case of unexpected error

--- providers/jdbc/shedlock-provider-exposed/src/main/kotlin/net/javacrumbs/shedlock/provider/exposed/ExposedStorageAccessor.kt
+++ providers/jdbc/shedlock-provider-exposed/src/main/kotlin/net/javacrumbs/shedlock/provider/exposed/ExposedStorageAccessor.kt
@@ -1,10 +1,13 @@
 package net.javacrumbs.shedlock.provider.exposed
 
-import java.sql.SQLException
 import java.sql.SQLIntegrityConstraintViolationException
 import java.time.Duration
 import java.time.LocalDateTime
 import net.javacrumbs.shedlock.core.LockConfiguration
+import net.javacrumbs.shedlock.provider.exposed.Shedlock.lockUntil
+import net.javacrumbs.shedlock.provider.exposed.Shedlock.lockedAt
+import net.javacrumbs.shedlock.provider.exposed.Shedlock.lockedBy
+import net.javacrumbs.shedlock.provider.exposed.Shedlock.name
 import net.javacrumbs.shedlock.support.AbstractStorageAccessor
 import net.javacrumbs.shedlock.support.LockException
 import org.jetbrains.exposed.sql.Case
@@ -12,7 +15,7 @@ import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.Expression
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.greater
 import org.jetbrains.exposed.sql.and
-import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.insertIgnore
 import org.jetbrains.exposed.sql.javatime.CurrentDateTime
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.jetbrains.exposed.sql.update
@@ -24,31 +27,31 @@ internal class ExposedStorageAccessor(private val database: Database) : Abstract
     override fun insertRecord(lockConfiguration: LockConfiguration): Boolean =
         transaction(database) {
             try {
-                Shedlock.insert {
-                    it[name] = lockConfiguration.name
-                    it[lockUntil] = nowPlus(lockConfiguration.lockAtMostFor)
-                    it[lockedAt] = now
-                    it[lockedBy] = hostname
-                }
-                true
+                Shedlock.insertIgnore {
+                        it[name] = lockConfiguration.name
+                        it[lockUntil] = nowPlus(lockConfiguration.lockAtMostFor)
+                        it[lockedAt] = now
+                        it[lockedBy] = hostname
+                    }
+                    .insertedCount > 0
             } catch (_: SQLIntegrityConstraintViolationException) {
                 // lock record already exists
                 false
-            } catch (e: SQLException) {
+            } catch (e: Exception) {
                 logger.debug("Exception thrown when inserting record", e)
-                false
+                throw LockException("Unexpected exception when locking", e)
             }
         }
 
     override fun updateRecord(lockConfiguration: LockConfiguration): Boolean =
         transaction(database) {
             try {
-                Shedlock.update({ (Shedlock.name eq lockConfiguration.name) and (Shedlock.lockUntil lessEq now) }) {
+                Shedlock.update({ (name eq lockConfiguration.name) and (lockUntil lessEq now) }) {
                     it[lockUntil] = nowPlus(lockConfiguration.lockAtMostFor)
                     it[lockedAt] = now
                     it[lockedBy] = hostname
                 } > 0
-            } catch (e: SQLException) {
+            } catch (e: Exception) {
                 throw LockException("Unexpected exception when locking", e)
             }
         }
@@ -58,10 +61,10 @@ internal class ExposedStorageAccessor(private val database: Database) : Abstract
             try {
                 val lockAtLeastUntil = Shedlock.lockedAt.plus(lockConfiguration.lockAtLeastFor)
 
-                Shedlock.update({ (Shedlock.name eq lockConfiguration.name) and (Shedlock.lockedBy eq hostname) }) {
+                Shedlock.update({ (name eq lockConfiguration.name) and (lockedBy eq hostname) }) {
                     it[lockUntil] = Case().When(lockAtLeastUntil greater now, lockAtLeastUntil).Else(now)
                 }
-            } catch (e: SQLException) {
+            } catch (e: Exception) {
                 throw LockException("Unexpected exception when unlocking", e)
             }
         }
@@ -71,13 +74,11 @@ internal class ExposedStorageAccessor(private val database: Database) : Abstract
         transaction(database) {
             try {
                 Shedlock.update({
-                    (Shedlock.name eq lockConfiguration.name) and
-                        (Shedlock.lockedBy eq hostname) and
-                        (Shedlock.lockUntil greater now)
+                    (name eq lockConfiguration.name) and (lockedBy eq hostname) and (lockUntil greater now)
                 }) {
                     it[lockUntil] = nowPlus(lockConfiguration.lockAtMostFor)
                 } > 0
-            } catch (e: SQLException) {
+            } catch (e: Exception) {
                 throw LockException("Unexpected exception when extending", e)
             }
         }
