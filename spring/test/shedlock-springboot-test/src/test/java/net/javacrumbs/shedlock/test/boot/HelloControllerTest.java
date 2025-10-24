5114553ceb866226d9d718be32c56b332bdd7f52
Lukas Krecan <lukas@krecan.net>
2025-10-24 18:38:53 +0200

    Revert "Upgrade SB to 7.0.0-RC1"
    This reverts commit c8cacc9b2fa21720a0aef37989cd4a0911b579f0.

--- spring/test/shedlock-springboot-test/src/test/java/net/javacrumbs/shedlock/test/boot/HelloControllerTest.java
+++ spring/test/shedlock-springboot-test/src/test/java/net/javacrumbs/shedlock/test/boot/HelloControllerTest.java
@@ -4,13 +4,15 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+@WebMvcTest(HelloController.class)
 class HelloControllerTest {
 
-    private final MockMvc mockMvc =
-            MockMvcBuilders.standaloneSetup(new HelloController()).build();
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
     void shouldCallController() throws Exception {
