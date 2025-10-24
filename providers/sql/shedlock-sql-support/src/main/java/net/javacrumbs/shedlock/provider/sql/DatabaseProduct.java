f7ae3e633c6eaeb7f043dccdb22bc7e9a50759a7
Lukas Krecan <lukas@krecan.net>
2025-10-24 12:36:18 +0200

    Refactor DatabaseProduct.matchProductName

--- providers/sql/shedlock-sql-support/src/main/java/net/javacrumbs/shedlock/provider/sql/DatabaseProduct.java
+++ providers/sql/shedlock-sql-support/src/main/java/net/javacrumbs/shedlock/provider/sql/DatabaseProduct.java
@@ -1,31 +1,24 @@
 package net.javacrumbs.shedlock.provider.sql;
 
-import java.util.Arrays;
 import java.util.function.Function;
-import java.util.function.Predicate;
 import org.jspecify.annotations.Nullable;
 
 public enum DatabaseProduct {
-    POSTGRES_SQL("PostgreSQL"::equalsIgnoreCase, PostgresSqlServerTimeStatementsSource::new),
-    SQL_SERVER("Microsoft SQL Server"::equalsIgnoreCase, MsSqlServerTimeStatementsSource::new),
-    ORACLE("Oracle"::equalsIgnoreCase, OracleServerTimeStatementsSource::new),
-    MY_SQL("MySQL"::equalsIgnoreCase, MySqlServerTimeStatementsSource::new),
-    MARIA_DB("MariaDB"::equalsIgnoreCase, MySqlServerTimeStatementsSource::new),
-    HQL("HSQL Database Engine"::equalsIgnoreCase, HsqlServerTimeStatementsSource::new),
-    H2("H2"::equalsIgnoreCase, H2ServerTimeStatementsSource::new),
-    DB2(s -> s.substring(0, 3).equalsIgnoreCase("DB2"), Db2ServerTimeStatementsSource::new),
-    UNKNOWN(s -> false, configuration -> {
+    POSTGRES_SQL(PostgresSqlServerTimeStatementsSource::new),
+    SQL_SERVER(MsSqlServerTimeStatementsSource::new),
+    ORACLE(OracleServerTimeStatementsSource::new),
+    MY_SQL(MySqlServerTimeStatementsSource::new),
+    MARIA_DB(MySqlServerTimeStatementsSource::new),
+    HQL(HsqlServerTimeStatementsSource::new),
+    H2(H2ServerTimeStatementsSource::new),
+    DB2(Db2ServerTimeStatementsSource::new),
+    UNKNOWN(configuration -> {
         throw new UnsupportedOperationException("DB time is not supported for unknown database product");
     });
 
-    private final Predicate<@Nullable String> productMatcher;
-
     private final Function<SqlConfiguration, SqlStatementsSource> serverTimeStatementsSource;
 
-    DatabaseProduct(
-            Predicate<String> productMatcher,
-            Function<SqlConfiguration, SqlStatementsSource> serverTimeStatementsSource) {
-        this.productMatcher = productMatcher;
+    DatabaseProduct(Function<SqlConfiguration, SqlStatementsSource> serverTimeStatementsSource) {
         this.serverTimeStatementsSource = serverTimeStatementsSource;
     }
 
@@ -37,13 +30,37 @@ public enum DatabaseProduct {
      * Searches for the right DatabaseProduct based on the ProductName returned from
      * JDBC Connection Metadata
      *
-     * @param productName Obtained from the JDBC connection. See java.sql.connection.getMetaData().getProductName().
+     * @param jdbcProductName Obtained from the JDBC connection. See java.sql.connection.getMetaData().getProductName().
      * @return The matching ProductName enum
      */
-    public static DatabaseProduct matchProductName(@Nullable String productName) {
-        return Arrays.stream(DatabaseProduct.values())
-                .filter(databaseProduct -> databaseProduct.productMatcher.test(productName))
-                .findFirst()
-                .orElse(UNKNOWN);
+    public static DatabaseProduct matchProductName(@Nullable String jdbcProductName) {
+        if (jdbcProductName == null) {
+            return UNKNOWN;
+        }
+        if ("PostgreSQL".equalsIgnoreCase(jdbcProductName)) {
+            return POSTGRES_SQL;
+        }
+        if ("Microsoft SQL Server".equalsIgnoreCase(jdbcProductName)) {
+            return SQL_SERVER;
+        }
+        if ("Oracle".equalsIgnoreCase(jdbcProductName)) {
+            return ORACLE;
+        }
+        if ("MySQL".equalsIgnoreCase(jdbcProductName)) {
+            return MY_SQL;
+        }
+        if ("MariaDB".equalsIgnoreCase(jdbcProductName)) {
+            return MARIA_DB;
+        }
+        if ("HSQL Database Engine".equalsIgnoreCase(jdbcProductName)) {
+            return HQL;
+        }
+        if ("H2".equalsIgnoreCase(jdbcProductName)) {
+            return H2;
+        }
+        if (jdbcProductName.regionMatches(true, 0, "DB2", 0, 3)) {
+            return DB2;
+        }
+        return UNKNOWN;
     }
 }
