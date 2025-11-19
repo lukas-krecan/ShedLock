9bd858d52a5f6ab6e05efb08eebf670e620939c4
Lukas Krecan <lukas@krecan.net>
2025-11-19 19:24:42 +0100

    #2899 Use SYSUTCDATETIME for MsSql in Exposed

--- providers/jdbc/shedlock-provider-exposed/src/test/kotlin/net/javacrumbs/shedlock/provider/exposed/MsSqlExposedLockProviderIntegrationTest.kt
+++ providers/jdbc/shedlock-provider-exposed/src/test/kotlin/net/javacrumbs/shedlock/provider/exposed/MsSqlExposedLockProviderIntegrationTest.kt
@@ -5,9 +5,6 @@ import net.javacrumbs.shedlock.test.support.jdbc.MsSqlServerConfig
 class MsSqlExposedLockProviderIntegrationTest : AbstractExposedLockProviderIntegrationTest(dbConfig = DB_CONFIG) {
 
     private companion object {
-        private val DB_CONFIG =
-            object : MsSqlServerConfig() {
-                override fun nowExpression(): String = "CURRENT_TIMESTAMP"
-            }
+        private val DB_CONFIG = MsSqlServerConfig()
     }
 }
