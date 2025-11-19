e64f71061bdbaa36cde4b37bc244db2f99abfdca
Andrei Purcaru <andrei.purcaru@teampicnic.com>
2025-11-19 14:26:38 +0100

    Format

--- providers/redis/shedlock-support-redis/src/main/java/net/javacrumbs/shedlock/provider/redis/support/InternalRedisLockTemplate.java
+++ providers/redis/shedlock-support-redis/src/main/java/net/javacrumbs/shedlock/provider/redis/support/InternalRedisLockTemplate.java
@@ -10,7 +10,8 @@ public interface InternalRedisLockTemplate {
 
     boolean setIfPresent(String key, String value, long expirationMs);
 
-    @Nullable Object eval(String script, String key, String... values);
+    @Nullable
+    Object eval(String script, String key, String... values);
 
     void delete(String key);
 }
